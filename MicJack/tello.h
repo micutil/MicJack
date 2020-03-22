a0cfbb4bfdef7e8a0c0ce4bcee50e078deecd2e0
Micono <micono@mac.com>
2020-03-22 13:07:25 +0900

    v1.1.0b2

---
+++ MicJack/tello.h
@@ -0,0 +1,51 @@
+#ifndef __TELLO_H__
+#define __TELLO_H__
+
+#include <Arduino.h>
+
+//-------------
+// Fundermental
+
+enum {
+  kCmd=0,
+  kState,
+  kVideo  
+};
+
+bool tello_udp_start(int n, bool doCmd=false);
+void tello_udp_stop(int n);
+
+void Break_Tello();
+void Wait_Tello_Res(int n);
+void Tello_UDP_Read(int n);
+
+//-------
+// Queue
+
+typedef struct telloq {
+  uint32 w;
+  String c;
+  telloq *n;
+  telloq *p;
+} ;
+
+void tello_setup();
+void tello_loop();
+
+void initQueue();
+void clearQueue();
+telloq *addQueue(String c, uint32 w);
+telloq *appendQueue(String c);
+void runQueue();
+void actionQueue();
+void actionEmergency();
+
+void Tello_Queue_Command(String msg);
+
+//---------
+// Command
+
+void Tello_UDP_Write(String msg);
+void Tello_Direct_Command(String msg);
+
+#endif //__TELLO_H__
